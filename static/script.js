15ddf77b5b17073bfdbc317b46e099d60b5d209d
johnmusiu <johnmusiu@gmail.com>
2018-05-25 18:40:18 +0300

    [feature #157787790] Added admin review to approve or reject request

--- static/script.js
+++ static/script.js
@@ -56,4 +56,61 @@ function toggleRequestForm(){
     }    
 }
 
+
+function toggleRequestDetails(id){
+    //get table on page by htmltag, 0 gets obj of the first table in the page
+    const table = document.getElementsByTagName("table")[0];
+    // from table obj get tr rows list
+    const rows = table.getElementsByTagName("tr"); // 
+    //loop through rows list
+    for(var i = 1; i < rows.length; i++){
+        // row object
+        var row = rows[i];
+        // Track with onclick(a row with a clicked event will trigger this)
+        row.onclick = function(){
+            var rowIndex = this.rowIndex;
+            insertDescRow(table, rows, rowIndex, id);            
+        }
+    }
+}
+
+//create row for request description
+function insertDescRow(table, rows, rowIndex, id){
+    const row = rows[rowIndex];
+    console.log((rowIndex+1)+" "+rows.length)
+    //if not last row, check if row should be added or deleted
+    if(rows.length !== rowIndex+1){
+        const nextRow = rows[rowIndex+1];
+        if(nextRow.classList.contains("shown")){
+            //delete that row
+            table.deleteRow(rowIndex+1);
+            stop();
+        }else{
+            createRow(table, rowIndex, row);
+        }
+    }else{
+        createRow(table, rowIndex, row);    
+    }
+    openIcon.classList.add('');
+    openIcon.classList.remove('');
+}
+
+function createRow(table, rowIndex, row){
+    const newRow = table.insertRow(rowIndex+1);
+    const rowData = row.getElementsByTagName("td");
+    newRow.classList.add("shown");
+    //insert and colspam cells
+    const tdataIndex = newRow.insertCell(0);
+    // tdataIndex.colSpan = 2;
+    const tdataDetails = newRow.insertCell(1);
+    //insert tags into table
+    tdataDetails.colSpan = 8;
+    tdataIndex.innerText = "";
+    tdataDetails.innerHTML = `<p> Name: <strong>${rowData[1].innerText}</strong> </p>`;
+    tdataDetails.innerHTML += `<p> Time requested: <strong>${rowData[4].innerText}</strong> </p>`;
+    tdataDetails.innerHTML += `<p> Description: <strong>${rowData[3].innerText}</strong> </p>`;
+    tdataDetails.innerHTML += `<p> Status: <strong>${rowData[6].innerText}</strong></p>`;
+    
+
+}
 showSlide(1); 
