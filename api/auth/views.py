27b2fe422cd21d02b3124221b88b4cae791054b5
johnmusiu <johnmusiu@gmail.com>
2018-06-03 12:41:12 +0300

    [feature #158054861] Move user input validation out of the route methods ie, login and register

--- api/auth/views.py
+++ api/auth/views.py
@@ -12,46 +12,20 @@ def register():
     email = str(request.data.get('email', ''))
     password = str(request.data.get('password', ''))
     confirm_password = str(request.data.get('confirm_password', ''))
+    
+    #returns True if all fields are properly filled
+    validation = register_validation(name, email, password, confirm_password)
 
-    if not name:
-        return jsonify({'message': 'Please fill in required name field!'}), 400
-    elif not name.replace(" ", "").isalpha():
-        return jsonify({
-            'message': "Please enter a valid name!",
-            'info': "A valid name has letters and spaces"
-        }), 400
-    if not email:
-        return jsonify({'message': 'Please fill in required email field!'}), 400
-    elif not re.match(r"(^[a-zA-Z_][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.][a-zA-Z]+$)", email):
-        return jsonify({
-            'message': 'Please enter a valid email address!'
-        }), 400
+    if validation is not True:
+        return validation
 
-    if not password:
-        return jsonify({
-            'message': 'Please fill in required password field!'
-            }), 400
-    elif not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}", password):
-        return jsonify({
-            'message': 'Please enter a strong password to signup!',
-            'info': 'Password must contain uppercase and lowercase letters, digits and be have min-lenght of 6'
-        }), 400
-    if not confirm_password:
-        return jsonify({
-            'message': 'Please fill in required confirm_password field!'
-            }), 400
-    if password != confirm_password:
-        return jsonify({
-            'message': 'Password and confirm password mismatch'
-        }), 400
-    # now save user to db
+   # now save user to db
     user = User(name, email, password)
     result = user.signup()
     if result[0] == "0":
         return jsonify({
             'message': result[1]
         }), 400
-    
     return jsonify({
         'message': 'Registration successful',
         'user_id': result[1][0],
@@ -59,27 +33,75 @@ def register():
         'name': result[1][2]
     }), 201
 
+def register_validation(name, email, password, confirm_password):
+    """ function to check if user fills in required data on sign up """
+    response = True
+
+    #check for null fields
+    if not name or not email or not password or not confirm_password:
+        if not name:
+            response = jsonify({
+                'message': 'Please fill in required name field!'
+            })
+        if not email:
+            response = jsonify({
+                'message': 'Please fill in required email field!'
+            })
+        if not password:
+            response = jsonify({
+                'message': 'Please fill in required password field!'
+            })
+        if not confirm_password:
+            response = jsonify({
+                'message': 'Please fill in required confirm_password field!'
+            })
+        if response is not True:
+            response.status_code = 400
+        return response
+    # check if provided data is valid
+    else:
+        if not name.replace(" ", "").isalpha():
+            response = jsonify({
+                'message': "Please enter a valid name!",
+                'info': "A valid name has only letters and spaces."
+            })
+    
+        if not re.match(
+                r"(^[a-zA-Z_][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.][a-zA-Z]+$)",
+                email):
+            response = jsonify({
+                'message': 'Please enter a valid email address!'
+            })
+ 
+        if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}", password):
+            response = jsonify({
+                'message': 'Please enter a strong password to signup!',
+                'info': 'Password must contain uppercase and lowercase letters,\
+                        digits and be have min-lenght of 6'
+        })
+        if response is not True:
+            response.status_code = 400
+        return response
+    # check if password matches confirm password
+    if password != confirm_password:
+        response = jsonify({
+            'message': 'Password and confirm password mismatch'
+        })
+        response.status_code = 400
+    
+    return response
+ 
 @auth.route('/api/v1/auth/login', methods=['POST'])
 def login():
     """ define user login and token issuance """
     email = str(request.data.get('email', ''))
     password = str(request.data.get('password', ''))
 
-    if not email:
-        response = jsonify({'message': 'Please fill in the email field'})
-        response.status_code = 400
-        return response
-    elif not re.match(r"(^[a-zA-Z_][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.][a-zA-Z]+$)", email):
-        response =  jsonify({
-            'message': 'Please enter a valid email address!'
-        })
-        response.status_code = 400
-        return response
-    if not password:
-        response = jsonify({'message': 'Please fill in the password field'})
-        response.status_code = 400
-        return response
-    
+    # return True if valid sign in data provided
+    validation = login_validation(email, password)
+    if validation is not True:
+        return validation
+
     user = User().users.get(email, False)
     if user is False:
         return jsonify({
@@ -97,3 +119,22 @@ def login():
             return jsonify({
                 'message': "Wrong login credentials provided!"
             }), 202
+
+def login_validation(email, password):
+    """ method to validate login user provided data """
+    if not email:
+        response = jsonify({'message': 'Please fill in the email field'})
+        response.status_code = 400
+        return response
+    elif not re.match(r"(^[a-zA-Z_][a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.][a-zA-Z]+$)", email):
+        response =  jsonify({
+            'message': 'Please enter a valid email address!'
+        })
+        response.status_code = 400
+        return response
+    if not password:
+        response = jsonify({'message': 'Please fill in the password field'})
+        response.status_code = 400
+        return response
+    
+    return True
