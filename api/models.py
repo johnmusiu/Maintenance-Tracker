70aca72dd99920bd25b7ac6a4a288563b518d1c6
johnmusiu <johnmusiu@gmail.com>
2018-06-09 05:38:44 +0300

    [chore #158221361] Create classes for admin and normal user

--- api/models.py
+++ api/models.py
@@ -74,108 +74,55 @@ class User():
         except Exception as e:
             return e
 
-
-class Request():
-    """ the requests model """
-
+class Admin(User):
+    """ admin user class """
     def __init__(self):
-        """initialize instance variables """
-
-    def save(self, category, title, description):
-        """ save request """
-        # check if a similar open or pending request exists for the user
-        """ get my requests """
-        db = DBConnect()
-        cursor = db.connect()
-        # check if email taken
-        try:
-            cursor.execute(u"SELECT * FROM requests where user_id = %s and \
-                            title = %s and (status = 'pending' \
-                            or status = 'open') and type = %s;",
-                           (session['user_id'], title, category,))
-            requests = cursor.fetchone()
-
-            if requests:
-                result = (False,
-                          "Request already exists, wait for resolution!")
-            else:
-                cursor.execute(u"INSERT INTO requests(user_id, type, title,\
-                            description, created_at, updated_at, status)\
-                           VALUES(%s, %s, %s, %s, current_timestamp, \
-                           current_timestamp, 'open') \
-                           RETURNING request_id;",
-                               (session['user_id'], category, title, description,))
-                db.conn.commit()
-                res_id = cursor.fetchone()[0]
-                result = (True, res_id)
-        except Exception:
-            result = (False)
-        return result
-
-    def get_all_my_requests(self, user_id):
-        """ get my requests """
-        sql = "SELECT * FROM requests where user_id = %s;"
-        values = (user_id,)
-        requests = fetch_all(sql, values)
-
-        if not requests:
-            result = False
-        else:
-            result = {}
-            for request in requests:
-                result[request[0]] = {
-                    "title": request[5],
-                    "description": request[6],
-                    "type": request[4],
-                    "status": request[3],
-                    "resolved_at": request[9],
-                    "created_at": request[7]
-                }
-        return result
-
-    def update(self, request_id, title, description, category):
-        """ update request """
-        # check if request exists
-        sql = u"SELECT * FROM requests where request_id = %s;"
+        """ inherit user methods """
+        User.__init__(self)
+    
+    def approve(self, request_id):
+        """ admin approve request"""
+        sql = u"UPDATE requests SET status='pending' WHERE request_id = %s \
+                RETURNING ('request_id');"
         values = (request_id,)
-        res = fetch_one(sql, values)
-        if not res:
-            result = (False, "This request does not exist!", 404)
-        else:
-            if res[1] != session['user_id']:
-                result = (
-                    False, "You can only update your own requests", 401)
-            elif res[3] != 'open':
-                result = (
-                    False, "Editing this request is not allowed", 403)
-            else:
-                sql = u"UPDATE requests SET title=%s, description=%s,\
-                        type=%s, updated_at=current_timestamp \
-                        WHERE request_id=%s;"
-                values = (title, description, category, request_id,)
-                res = execute_query(sql, values)
-                result = (True, values)
-        return result
-
-    def get_by_id(self, request_id):
-        """ get request by id"""
-        sql = u"SELECT * FROM requests WHERE request_id = %s AND user_id = %s;"
-        values = (request_id, session['user_id'],)
-        request = fetch_one(sql, values)
+        request = execute_query(sql, values)
         if not request:
-            return (False, "Request id not found.", 404)
+            return (False, "This request does not exist!", 404)
         result = {
-            "title": request[5],
-            "description": request[6],
-            "type": request[4],
-            "status": request[3],
-            "resolved_at": request[9],
-            "created_at": request[7]
+            "message": "Request approved successfully",
+            "request_id": request
         }
         return (True, result)
 
-    @staticmethod
-    def admin_get_all():
+    def disapprove(self, request_id):
+        """ admin disapprove request """
+        sql = u"UPDATE requests SET status='disapproved' WHERE request_id = %s \
+                RETURNING ('request_id');"
+        values = (request_id,)
+        request = execute_query(sql, values)
+        if not request:
+            return (False, "This request does not exist!", 404)
+        result = {
+            "message": "Request disapproved successfully",
+            "request_id": request
+        }
+        return (True, result)
+
+    def resolve(self, request_id):
+        """ admin resolve request"""
+        sql = u"UPDATE requests SET status='resolved' WHERE request_id = %s \
+                RETURNING ('request_id');"
+        values = (request_id,)
+        request = execute_query(sql, values)
+        if not request:
+            return (False, "This request does not exist!", 404)
+        result = {
+            "message": "Request resolved successfully",
+            "request_id": request
+        }
+        return (True, result)
+
+    def get_all_requests(self):
         """ get request by id"""
         sql = u"SELECT * FROM requests;"
         requests = fetch_all(sql)
@@ -193,8 +140,7 @@ class Request():
             }
         return (True, result)
 
-    @staticmethod
-    def admin_get_by_id(request_id):
+    def get_request_by_id(self, request_id):
         """ get request by id"""
         sql = u"SELECT * FROM requests WHERE request_id = %s;"
         values = (request_id,)
@@ -210,48 +156,130 @@ class Request():
             "created_at": request[7]
         }
         return (True, result)
+    
+class NormalUser(User):
+    """ normal user class ie. has no admin priviledges """
+    def __init__(self):
+        User.__init__(self)
+    
+    def make_request(self, title, description, category):
+        """ user create request """
+        request = Request(title, description, category)
+        return request.create()
 
-    @staticmethod
-    def admin_approve(request_id):
-        """ admin approve request"""
-        sql = u"UPDATE requests SET status='pending' WHERE request_id = %s \
-                RETURNING ('request_id');"
-        values = (request_id,)
-        request = execute_query(sql, values)
-        if not request:
-            return (False, "This request does not exist!", 404)
-        result = {
-            "message": "Request approved successfully",
-            "request_id": request
-        }
-        return (True, result)
+    def update_request(self, request_id, title, description, category):
+        """ user update request """
+        request = Request(title, description, category, request_id)
+        return request.update()
+        
+    def get_requests(self):
+        """ get user requests """
+        return Request().get_user_requests()
 
-    @staticmethod
-    def admin_disapprove(request_id):
-        """ admin disapprove request """
-        sql = u"UPDATE requests SET status='disapproved' WHERE request_id = %s \
-                RETURNING ('request_id');"
-        values = (request_id,)
-        request = execute_query(sql, values)
-        if not request:
-            return (False, "This request does not exist!", 404)
-        result = {
-            "message": "Request disapproved successfully",
-            "request_id": request
-        }
-        return (True, result)
+    def get_request_by_id(self, request_id):
+        """ get user request by id """
+        return Request().get_by_id(request_id)
+        
+class Request():
+    """ the requests model """
 
-    @staticmethod
-    def admin_resolve(request_id):
-        """ admin resolve request"""
-        sql = u"UPDATE requests SET status='resolved' WHERE request_id = %s \
-                RETURNING ('request_id');"
-        values = (request_id,)
-        request = execute_query(sql, values)
+    def __init__(self, title=None, description=None, category=None, request_id=None):
+        """initialize instance variables """
+        self.title = title
+        self.description = description
+        self.category = category
+        self.request_id = request_id
+
+    def create(self):
+        """ save request """
+        # check if a similar open or pending request exists for the user
+        db = DBConnect()
+        cursor = db.connect()
+        # check if email taken
+        try:
+            sql = u"SELECT * FROM requests where user_id = %s and \
+                            title = %s and (status = 'pending' \
+                            or status = 'open') and type = %s;"
+            values = (session['user_id'], self.title, self.category,)
+            requests = fetch_one(sql, values)
+
+            if requests:
+                result = (False,
+                          "Request already exists, wait for resolution!")
+            else:
+                cursor.execute(u"INSERT INTO requests(user_id, type, title,\
+                            description, created_at, updated_at, status)\
+                           VALUES(%s, %s, %s, %s, current_timestamp, \
+                           current_timestamp, 'open') \
+                           RETURNING request_id;",
+                               (session['user_id'], self.category, self.title, 
+                               self.description,))
+                db.conn.commit()
+                res_id = cursor.fetchone()[0]
+                result = (True, res_id)
+        except Exception:
+            result = (False)
+        return result
+
+    def get_user_requests(self):
+        """ get my requests """
+        sql = "SELECT * FROM requests where user_id = %s;"
+        values = (session['user_id'],)
+        requests = fetch_all(sql, values)
+
+        if not requests:
+            result = False
+        else:
+            result = {}
+            for request in requests:
+                result[request[0]] = {
+                    "title": request[5],
+                    "description": request[6],
+                    "type": request[4],
+                    "status": request[3],
+                    "resolved_at": request[9],
+                    "created_at": request[7]
+                }
+        return result
+
+    def update(self):
+        """ update request """
+        # check if request exists
+        sql = u"SELECT * FROM requests where request_id = %s;"
+        values = (self.request_id,)
+        res = fetch_one(sql, values)
+        if not res:
+            result = (False, "This request does not exist!", 404)
+        else:
+            if res[1] != session['user_id']:
+                result = (
+                    False, "You can only update your own requests", 401)
+            elif res[3] != 'open':
+                result = (
+                    False, "Editing this request is not allowed", 403)
+            else:
+                sql = u"UPDATE requests SET title=%s, description=%s,\
+                        type=%s, updated_at=current_timestamp \
+                        WHERE request_id=%s;"
+                values = (self.title, self.description, self.category, 
+                            self.request_id,)
+                res = execute_query(sql, values)
+                result = (True, values)
+        return result
+
+    def get_by_id(self, request_id):
+        """ get user request by id"""
+        sql = u"SELECT * FROM requests WHERE request_id = %s AND user_id = %s;"
+        values = (request_id, session['user_id'],)
+        request = fetch_one(sql, values)
         if not request:
-            return (False, "This request does not exist!", 404)
+            return (False, "Request id not found.", 404)
         result = {
-            "message": "Request resolved successfully",
-            "request_id": request
+            "title": request[5],
+            "description": request[6],
+            "type": request[4],
+            "status": request[3],
+            "resolved_at": request[9],
+            "created_at": request[7]
         }
         return (True, result)
--- api/mrequests/admin.py
+++ api/mrequests/admin.py
@@ -1,17 +1,17 @@
 # api/requests/admin.py
 
-from . import admin
+from . import admin_bp
 from flask import jsonify
-from api.models import Request
+from api.models import Admin
 from ..wrappers import token_required, role_required
 
 
-@admin.route('', methods=['GET'])
+@admin_bp.route('', methods=['GET'])
 @token_required
 @role_required('1')
 def requests():
     # returns all requests
-    result = Request().admin_get_all()
+    result = Admin().get_all_requests()
     if result is False:
         return jsonify(
             {"message": "There are no requests yet!"}), 404
@@ -19,12 +19,12 @@ def requests():
     return response, 200
 
 
-@admin.route('/<int:request_id>', methods=['GET'])
+@admin_bp.route('/<int:request_id>', methods=['GET'])
 @token_required
 @role_required('1')
 def request_by_id(request_id):
     # returns all requests
-    result = Request().admin_get_by_id(request_id)
+    result = Admin().get_request_by_id(request_id)
     if result is False:
         return jsonify(
             {"message": "There are no requests yet!"}), 404
@@ -32,12 +32,12 @@ def request_by_id(request_id):
     return response, 200
 
 
-@admin.route('/<int:request_id>/approve', methods=['PUT'])
+@admin_bp.route('/<int:request_id>/approve', methods=['PUT'])
 @token_required
 @role_required('1')
 def approve(request_id):
     # returns all requests
-    result = Request().admin_approve(request_id)
+    result = Admin().approve(request_id)
     if result is False:
         return jsonify(
             {"message": "There are no requests yet!"}), 404
@@ -45,12 +45,12 @@ def approve(request_id):
     return response, 200
 
 
-@admin.route('/<int:request_id>/disapprove', methods=['PUT'])
+@admin_bp.route('/<int:request_id>/disapprove', methods=['PUT'])
 @token_required
 @role_required('1')
 def disapprove(request_id):
     # returns all requests
-    result = Request().admin_disapprove(request_id)
+    result = Admin().disapprove(request_id)
     if result is False:
         return jsonify(
             {"message": "There are no requests yet!"}), 404
@@ -58,12 +58,12 @@ def disapprove(request_id):
     return response, 200
 
 
-@admin.route('/<int:request_id>/resolve', methods=['PUT'])
+@admin_bp.route('/<int:request_id>/resolve', methods=['PUT'])
 @token_required
 @role_required('1')
 def resolve(request_id):
     # returns all requests
-    result = Request().admin_resolve(request_id)
+    result = Admin().resolve(request_id)
     if result is False:
         return jsonify(
             {"message": "There are no requests yet!"}), 404
