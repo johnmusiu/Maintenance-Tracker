093cca2c41fa6261e7d60d5d72b9918bab4b2a71
johnmusiu <johnmusiu@gmail.com>
2018-06-09 10:31:39 +0300

    [chore #158221361] Add blueprint for super admin

--- api/mrequests/__init__.py
+++ api/mrequests/__init__.py
@@ -4,6 +4,6 @@ from flask import Blueprint
 
 mrequests = Blueprint('mrequests', __name__, url_prefix='/api/v1/users/requests')
 admin_bp = Blueprint('admin', __name__, url_prefix='/api/v1/requests')
+super_admin_bp =  Blueprint('super_admin', __name__, url_prefix='/api/v1/admin')
 
-
-from . import views, admin
+from . import views, admin, super_admin
