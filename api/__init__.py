2cede3171274af13f0224ccad317c2788805aa49
John Musiu <johnmusiu@gmail.com>
2018-06-23 11:49:23 +0300

    [chore 158382565] Enable CORS

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,14 @@
 """
 import os
 from flask_api import FlaskAPI
+from flask_cors import CORS
 from instance.config import app_config
 
 
 def create_app(config_name):
 
     app = FlaskAPI(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.secret_key = os.getenv('SECRET_KEY')
